dc7fabc953c6c53c85c168e2a9816932ac55369c
teerentt <85568586+teerentt@users.noreply.github.com>
2022-03-25 17:57:33 -0700

    part 2

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTU2NjczMTcwMjI2Njg4MDkw.YjzpUQ.spUxIlmlGX3LW8vq9v63DYigLgA"
+
+const TOKEN = "OTU2NjczMTcwMjI2Njg4MDkw.YjzpUQ.hRBf_RxWHacNCkiOB8t7DeDW4A0"
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
+
+
+
+const welcomeChannelId = "956696047634239548"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+
+})
+
+
 
 
 client.login(process.env.TOKEN)
